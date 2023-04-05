0a4d1d528793ee17ba74772402df11ac9c6b50c2
dox404 <muktarul.hoque@hotmail.com>
2023-04-05 15:58:13 +0530

    added db connection & created user model and conneted to forntend

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
-const express= require('express');
+const express = require('express');
+require('./db/config')
+const User = require('./db/User')
+
+const cors = require('cors')
 
 
-const App=express();
-const PORT=5000
+const App = express();
+const PORT = 5000
 
 
 
-App.get('/',(req,res)=>{
-res.send("Hello from server")
+App.use(express.json())
+App.use(cors())
+
+// get routes
+App.get('/', (req, res) => {
+    res.send("Hello from server")
 
 
 })
-App.listen(PORT,(req,res)=>{
+App.get('/login', (req, res) => {
+    res.send("Hello from login")
+
+
+})
+App.get('/signup', (req, res) => {
+    res.send("Hello from signup")
+
+
+})
+App.get('/about',(req,res)=>{
+    res.send("Hello from about")
+    
+
+
+    
+})
+
+//post routes
+
+App.post('/signup',async(req,res)=>{
+
+    let user=new User(req.body)
+   let result=await user.save()
+    // res.send("api is running")
+    res.send(result)
+    // console.log(req.body)
+})
+
+
+
+
+App.listen(PORT, (req, res) => {
     console.log(`sever is started on port ${PORT}`)
 })
 
