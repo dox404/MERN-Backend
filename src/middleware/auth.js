a90d4426a9c37064db96afa549202037c74dfe3d
dox404 <muktarul.hoque@hotmail.com>
2023-04-11 23:09:22 +0530

    written code in auth.js file

---
+++ src/middleware/auth.js
@@ -0,0 +1,20 @@
+
+const jwt=require("jsonwebtoken")
+const User=require("../models/User")
+
+const auth=async(req,res,next)=>{
+    try {
+        const token=req.cookies.jwt
+        const verifyUser=jwt.verify(token,"MYNAMEISMUKTARULHOQUE")
+        // console.log(verifyUser)
+       
+        next()
+        
+
+    } catch (error) {
+    //  res.status(401).send(error)
+    res.redirect('login')
+    }
+}
+
+module.exports=auth;
